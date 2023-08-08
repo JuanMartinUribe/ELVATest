8a477f78f4f734ca8a04b6a101767584c44714b2
unknown <jmuribef@eafit.edu.co>
2023-08-07 22:34:49 -0500

    update arc's variable names to make it cleaner

--- ArcController.py
+++ ArcController.py
@@ -4,17 +4,15 @@ import requests
 
 
 class ArcController:
-    def __init__(self,url) -> None:
-        self.data = requests.get(url).json()
-        self.features = []
+    def __init__(self, url) -> None:
+        self.data = requests.get(url).json().get("features")
 
     def coords_to_neighborhood(self, longitude: int, latitude: int) -> str:
-        if not self.features: self.features = self.data.get("features")
-        for feature in self.features:
+        for neighborhood in self.data:
             try:
-                coords = feature.get("geometry").get("coordinates")[0]
+                coords = neighborhood.get("geometry").get("coordinates")[0]
             except ValueError as e:
-                print('This feature coordinates array is not parsed properly',e)
+                print("This feature coordinates array is not parsed properly", e)
                 continue
             polygon_vertices = [tuple(coord) for coord in coords]
             point_coords = (longitude, latitude)
@@ -22,11 +20,11 @@ class ArcController:
                 polygon_vertices, point_coords
             )
             if is_inside_polygon:
-                return feature.get("properties").get("NAME")
+                return neighborhood.get("properties").get("NAME")
         return ""
 
     @staticmethod
-    def _is_point_inside_polygon(vertices: List, point_coords: List) -> bool:
-        polygon = Polygon(vertices)
+    def _is_point_inside_polygon(vortices: List, point_coords: List) -> bool:
+        polygon = Polygon(vortices)
         point = Point(point_coords)
         return point.within(polygon)
